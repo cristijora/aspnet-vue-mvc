d03e5bde4752370443ef6b3a362de84aea5d311e
cristijora <joracristi@gmail.com>
2017-08-12 23:30:45 +0300

    Initial commit

---
+++ VueMVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace VueMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message from Controller";
+            return View();
+        }
+    }
+}
