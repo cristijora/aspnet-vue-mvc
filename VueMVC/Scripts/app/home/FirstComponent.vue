611de72dc7699adf375727dadde2eaec94b2c740
cristijora <joracristi@gmail.com>
2017-08-13 01:29:43 +0300

    Add FirstComponent vue file

---
+++ VueMVC/Scripts/app/home/FirstComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="time">
+        {{ time }}
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            time: {
+                type: Object,
+                required: true, 
+            }
+        }
+    }
+</script>
+<style>
+    .time {
+        color: #F44336;
+    }
+</style>
