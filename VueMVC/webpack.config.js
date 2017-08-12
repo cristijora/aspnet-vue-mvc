6a3b635d95587689efde6757854a16f7be3a4d82
cristijora <joracristi@gmail.com>
2017-08-12 23:50:45 +0300

    Add package.json, webconfig and sample js file

---
+++ VueMVC/webpack.config.js
@@ -0,0 +1,84 @@
+var path = require('path')
+var webpack = require('webpack')
+var fs = require('fs')
+
+var appBasePath = './Scripts/app/'
+
+var jsEntries = {}
+// We search for index.js files inside basePath folder and make those as entries
+fs.readdirSync(appBasePath).forEach(function (name) {
+    var indexFile = appBasePath + name + '/index.js'
+    if (fs.existsSync(indexFile)) {
+        jsEntries[name] = indexFile
+    }
+})
+
+module.exports = {
+    entry: jsEntries,
+    output: {
+        path: path.resolve(__dirname, './Scripts/bundle/'),
+        publicPath: '/Scripts/bundle/',
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.join(__dirname, appBasePath)
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+                loader: 'file-loader',
+                query: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    devServer: {
+        proxy: {
+            '*': {
+                target: 'http://localhost:5001',
+                changeOrigin: true
+            }
+        }
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ])
+}
